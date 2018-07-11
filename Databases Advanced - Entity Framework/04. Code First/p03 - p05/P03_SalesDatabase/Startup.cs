828e1fcde0d0568e0f402ca106d7eb8e53315478
Nackey <s2k@abv.bg>
2018-07-11 15:26:23 +0300

    add more tasks

---
+++ Databases Advanced - Entity Framework/04. Code First/p03 - p05/P03_SalesDatabase/Startup.cs
@@ -0,0 +1,16 @@
+﻿using P03_SalesDatabase.Data;
+using System;
+
+namespace P03_SalesDatabase
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            using (SalesContext context = new SalesContext())
+            {
+
+            }
+        }
+    }
+}
